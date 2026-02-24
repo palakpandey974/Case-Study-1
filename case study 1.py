9138c2e5bb0b284f991fd90923224a6c5088a32d
palakpandey974 <pandeypalak974@gmail.com>
2026-02-24 14:50:55 +0530

    Add files via upload

---
+++ case study 1.py
@@ -0,0 +1,14 @@
+total = 0
+items=int(input("Enter number of items: "))
+for i in range(1,items+1):
+    price = float(input("Enter price: "))
+    items_quantity = int(input("Enter quantity: "))
+    total = total + (price *items_quantity)
+print("Original Total =", total)
+if total > 100:
+    discount = total * 0.10
+else:
+    discount = 0
+print("Discount =", discount)
+final_amount = total - discount
+print("Final Amount =", final_amount)
